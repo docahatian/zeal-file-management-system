6290387b21939825213c1f93c91aaf541260443a
ja-craming021602 <77001336+rjcagay-up@users.noreply.github.com>
2023-01-08 15:11:41 +0800

    Update zealtc_filemanagementdb.sql

--- db/zealtc_filemanagementdb.sql
+++ db/zealtc_filemanagementdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2023 at 02:24 PM
+-- Generation Time: Jan 08, 2023 at 08:09 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -29,21 +29,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `storage` (
   `store_id` int(11) NOT NULL,
+  `stud_category` varchar(20) NOT NULL,
   `filename` varchar(100) NOT NULL,
   `file_type` varchar(20) NOT NULL,
   `date_uploaded` varchar(100) NOT NULL,
   `stud_no` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `storage`
---
-
-INSERT INTO `storage` (`store_id`, `filename`, `file_type`, `date_uploaded`, `stud_no`) VALUES
-(3, 'WWW.YTS.AG.jpg', 'image/jpeg', '2019-01-30, 12:27 AM', 14523),
-(4, 'WWW.YTS.AG.jpg', 'image/jpeg', '2019-01-30, 12:34 AM', 14531),
-(7, 'aaa.pdf', 'application/pdf', '2020-10-11, 01:38 PM', 1353),
-(8, 'dbuser.accdb', 'application/msaccess', '2020-10-11, 01:38 PM', 1353);
 
 -- --------------------------------------------------------
 
@@ -67,7 +58,8 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`stud_id`, `stud_no`, `firstname`, `lastname`, `gender`, `course`, `status`, `password`) VALUES
-(6, 1353, 'Harry', 'Den', 'Male', '3B', '', '5f4dcc3b5aa765d61d8327deb882cf99');
+(6, 1353, 'Harry', 'Den', 'Male', '3B', '', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(7, 2020, 'Eugene', 'Labastida', 'Male', 'SMAW ', 'Graduate', '7b7a53e239400a13bd6be6c91c4f6c4e');
 
 -- --------------------------------------------------------
 
@@ -89,7 +81,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `password`, `status`) VALUES
-(1, 'Administrator', '', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'administrator');
+(1, 'Administrator', '', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'administrator'),
+(2, 'Rod ', 'Cagay ', 'admin2', 'c84258e9c39059a89ab77d846ddab909', 'Regular');
 
 --
 -- Indexes for dumped tables
@@ -121,19 +114,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `storage`
 --
 ALTER TABLE `storage`
-  MODIFY `store_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `store_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `stud_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `stud_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
