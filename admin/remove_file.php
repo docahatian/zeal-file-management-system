ea0845fd9c6741a2d61df7c32fcad79cab704122
ja-craming021602 <77001336+rjcagay-up@users.noreply.github.com>
2023-01-08 15:03:34 +0800

    file download

--- admin/remove_file.php
+++ admin/remove_file.php
@@ -5,9 +5,11 @@
 		$store_id = $_GET['store_id'];
 		$query = mysqli_query($conn, "SELECT * FROM `storage` WHERE `store_id` = '$store_id'") or die(mysqli_error());
 		$fetch  = mysqli_fetch_array($query);
-		$filename = $fetch['sub_type'];
+		$filename = $fetch['filename'];
+		$sub_type = $fetch['stud_category'];
 		$stud_no = $fetch['stud_no'];
-		if(unlink("../files/".$stud_no."/".$filename)){
+		if(unlink("../files/".$stud_no."/".$sub_type."/".$filename)){
+
 			mysqli_query($conn, "DELETE FROM `storage` WHERE `store_id` = '$store_id'") or die(mysqli_error());
 		}
 	}
