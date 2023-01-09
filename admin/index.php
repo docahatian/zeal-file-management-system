2de5dcb0df01b4316d03e94d81bbbaf615fcbb2a
ja-craming021602 <77001336+rjcagay-up@users.noreply.github.com>
2023-01-09 15:05:10 +0800

    modified html data

--- admin/index.php
+++ admin/index.php
@@ -22,7 +22,24 @@
 		<p>
 			Zeal Skills and Assesment is an institution accredittedby TESDA that offers Training Courses for Technical and Vocational Education (TVET) under the Technical Education and Skills Development Authority (TESDA)
             and the Higher Education(CHED).
-	   </p>
+	   </p>                     
+                       
+    <table>
+        <tr>
+            <th>TEC-VOC Courses Offered </th>
+            <th>Duration</th>
+       </tr>
+  <tr>
+    <td> Sheild Metal Arc Weldding (SMAW) NC II </td>
+    <td>   268 Hours</td>
+       </tr>
+       <tr>
+         <td> Machining NC II </td>
+         <td>377 Hours</td>
+      </tr>
+   </table>
+
+       
        <h3 class="section-subheading text-muted">Training Fees and Schedule </h3>
 		<p>
 			The cost of training and tuition fees may vary depending upon the specific course of interest and the school offering 
@@ -40,8 +57,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Contact Us</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h2 class="section-heading">Location and Contact Information</h2>
+                    <h3 class="section-subheading text-muted">To enroll in TESDA courses, please contact the school the school directly  at telephone number
+                        7528585.You can also visit their school/training center and inquire for other documents they may need.
+                    </h3>
                 </div>
             </div>
         </div>
