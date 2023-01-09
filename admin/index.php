b9af821610bfc82ac34e17e73a2bca2fa49c8b82
ja-craming021602 <77001336+rjcagay-up@users.noreply.github.com>
2023-01-09 14:45:56 +0800

    CSS format

--- admin/index.php
+++ admin/index.php
@@ -16,22 +16,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">About</h2>
+                    <h2 class="section-heading">About Us</h2>
 
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h3 class="section-subheading text-muted">Zeal Skills Training and Assessment Center</h3>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, omnis nemo reiciendis porro amet est sed ad eligendi possimus ex voluptate, numquam quam fugit hic ratione quod dolor optio enim.
-			Laudantium explicabo tempore temporibus officia ipsum unde odio neque! Voluptates culpa dolor explicabo in velit pariatur officiis, ipsa hic voluptas blanditiis doloribus reiciendis soluta beatae corrupti deleniti. Officia, maiores ipsa?
-			Omnis, architecto aut neque ipsum et rem eaque. Ad et tenetur, maxime sequi nobis quisquam possimus doloremque vitae, quis at reiciendis nihil magni voluptas suscipit repudiandae, molestias enim expedita. Quidem!
-			Quae consectetur aliquam qui officiis nesciunt voluptate quis magni eligendi ut expedita. Similique soluta beatae doloremque dolor distinctio modi minus eius iure corporis. Nam temporibus molestias commodi quae. Veniam, sapiente!
-			Unde repudiandae, debitis quas impedit dignissimos, saepe nobis optio expedita doloribus fugit perspiciatis nemo? Vel esse iure quia perspiciatis quis asperiores quo labore consectetur quos deserunt. Architecto cum nulla eius!
+			Zeal Skills and Assesment is an institution accredittedby TESDA that offers Training Courses for Technical and Vocational Education (TVET) under the Technical Education and Skills Development Authority (TESDA)
+            and the Higher Education(CHED).
 	   </p>
+       <h3 class="section-subheading text-muted">Training Fees and Schedule </h3>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur omnis quo nesciunt! Pariatur veritatis, ut rem, non autem omnis ab in esse tempore dolorem enim? Eligendi libero numquam voluptates labore!
-			Fugit magni atque reprehenderit aspernatur, alias placeat distinctio illo dolor optio animi nihil, doloremque ex nulla ipsam exercitationem eius dolorum deserunt officiis provident debitis tenetur enim et voluptatum. Vel, accusamus.
-			Modi, praesentium eveniet minima pariatur at nihil quam ad ullam iure aut iusto sint inventore dolor. Voluptatibus porro sint nulla, id, dolores quisquam accusamus quae eveniet aliquam assumenda modi esse?
-			Asperiores inventore eum praesentium officiis impedit optio! Quia a odio laudantium quae similique. Earum autem modi esse eius voluptates quis accusantium expedita sunt consequuntur molestias. Ipsum omnis itaque natus minus?
-			Nisi hic illo magni itaque quos reiciendis numquam dicta neque dolor. Quis voluptates nulla facere sed quas tempora dolorem? Dolorum est eaque eius porro corporis eum soluta reiciendis. Itaque, nemo?
+			The cost of training and tuition fees may vary depending upon the specific course of interest and the school offering 
+            and training. Training centers also may have slightly different syllbus to teach to 
+            students in class 
+
 	    </p>
                 </div>
             </div>
