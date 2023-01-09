7cc4e7bd2fa5823070012cf21290df37ddc11141
ja-craming021602 <77001336+rjcagay-up@users.noreply.github.com>
2023-01-09 15:45:29 +0800

    Update zealtc_filemanagementdb.sql

--- db/zealtc_filemanagementdb.sql
+++ db/zealtc_filemanagementdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 08, 2023 at 08:09 AM
+-- Generation Time: Jan 09, 2023 at 08:43 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -36,6 +36,13 @@ CREATE TABLE `storage` (
   `stud_no` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `storage`
+--
+
+INSERT INTO `storage` (`store_id`, `stud_category`, `filename`, `file_type`, `date_uploaded`, `stud_no`) VALUES
+(21, 'Registration', 'Cagay_CSRS.pdf', 'application/pdf', '2023-01-09, 01:27 PM', 2020);
+
 -- --------------------------------------------------------
 
 --
@@ -48,7 +55,7 @@ CREATE TABLE `student` (
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `gender` varchar(10) NOT NULL,
-  `course` varchar(5) NOT NULL,
+  `course` varchar(20) NOT NULL,
   `status` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -58,8 +65,9 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`stud_id`, `stud_no`, `firstname`, `lastname`, `gender`, `course`, `status`, `password`) VALUES
-(6, 1353, 'Harry', 'Den', 'Male', '3B', '', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(7, 2020, 'Eugene', 'Labastida', 'Male', 'SMAW ', 'Graduate', '7b7a53e239400a13bd6be6c91c4f6c4e');
+(7, 2020, 'Eugene', 'Labastida', 'Male', 'SMAW ', 'Graduate', '7b7a53e239400a13bd6be6c91c4f6c4e'),
+(8, 2021, 'Joey', 'Catalan', 'Male', 'Machi', 'Trainee', '05a5cf06982ba7892ed2a6d38fe832d6'),
+(9, 2023, 'Japh', ' Poledo', 'Male', 'SMAW ', 'Graduate', 'c84258e9c39059a89ab77d846ddab909');
 
 -- --------------------------------------------------------
 
@@ -82,7 +90,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `password`, `status`) VALUES
 (1, 'Administrator', '', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'administrator'),
-(2, 'Rod ', 'Cagay ', 'admin2', 'c84258e9c39059a89ab77d846ddab909', 'Regular');
+(2, 'Rod ', 'Cagay ', 'admin2', 'c84258e9c39059a89ab77d846ddab909', 'Regular'),
+(3, 'Renz', 'Cagay', 'admin2', 'c84258e9c39059a89ab77d846ddab909', 'Regular');
 
 --
 -- Indexes for dumped tables
@@ -114,19 +123,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `storage`
 --
 ALTER TABLE `storage`
-  MODIFY `store_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `store_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `stud_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `stud_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
